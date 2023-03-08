8080ca3ed92e5a7aa7e31da092fd132d3d5ae899
collo324 <emmanuellekishon@1998gmail.com>
2023-03-07 20:00:02 -0800

    task4

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Author - bamidele Adefolaju
+# Author -Bamidele Adefolaju
 
-def remove_char_at(str, n):
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
